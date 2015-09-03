cb475687e2a510aed5a5f72b94930ec92b9cbac2
jacek wojcik <jackfrompoland@op.pl>
2015-09-03 23:19:02 +0200

    dd

--- platforms/wp8/www/cordova_plugins.js
+++ platforms/wp8/www/cordova_plugins.js
@@ -1,19 +1,10 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/org.apache.cordova.battery-status/www/battery.js",
-        "id": "org.apache.cordova.battery-status.battery",
-        "clobbers": [
-            "navigator.battery"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.0.0",
-    "org.apache.cordova.geolocation": "0.3.12",
-    "org.apache.cordova.battery-status": "0.2.12"
+    "org.apache.cordova.geolocation": "0.3.12"
 }
 // BOTTOM OF METADATA
 });
